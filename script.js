f93d885badb0478d6ad90183ad41907de1c52243
Artem Nikolaev <artem.nicolaev@gmail.com>
2020-06-18 21:45:28 +0300

    Что-то напридумывал

--- script.js
+++ script.js
@@ -1,2 +1,46 @@
-console.log(1);
-console.log(1);
+'use strict';
+
+let isNumber = function(n) {
+    return !isNaN (parseFloat(n)) && isFinite(n);
+};
+
+function guessedNumber() {
+    const guessedNumber = 33;
+
+}
+
+let enteredNumber;
+
+function start() {
+    do {
+        enteredNumber = prompt('Угадай число от 1 до 100');
+    }
+    while(!isNumber(enteredNumber)); 
+};
+
+start();
+
+function startLess() {
+    do {
+        enteredNumber = prompt('Загаданное число меньше. Еще раз введите число');
+    }
+    while(!isNumber(enteredNumber)); 
+};
+
+function startMore() {
+    do {
+        enteredNumber = prompt('Загаданное число больше. Еще раз введите число');
+    }
+    while(!isNumber(enteredNumber)); 
+};
+
+
+function comparisonNumbers() {
+    if (enteredNumber > guessedNumber) {
+        startLess();
+    } else if (enteredNumber < guessedNumber) {
+        startMore();
+    } else alert("Поздравляю! Вы угадали число");
+};
+
+comparisonNumbers();
